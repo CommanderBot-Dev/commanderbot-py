8f40b85639338f969569e6dda424044838a76429
DoubleF3lix <48461905+Double-Felix@users.noreply.github.com>
2021-01-17 17:23:20 -0600

    Fixed wrong command name

--- commanderbot_ext/jira/jira_cog.py
+++ commanderbot_ext/jira/jira_cog.py
@@ -1,5 +1,5 @@
-import requests
 import discord
+import requests
 from commanderbot_lib.logging import Logger, get_clogger
 from discord.ext.commands import Bot, Cog, command
 
@@ -10,15 +10,15 @@ class JiraCog(Cog):
         self._log: Logger = get_clogger(self)
 
         self.resolution_table = {
-            "https://bugs.mojang.com/rest/api/2/resolution/1": "Fixed", 
-            "https://bugs.mojang.com/rest/api/2/resolution/2": "Won't Fix", 
-            "https://bugs.mojang.com/rest/api/2/resolution/3": "Duplicate", 
-            "https://bugs.mojang.com/rest/api/2/resolution/4": "Incomplete", 
-            "https://bugs.mojang.com/rest/api/2/resolution/5": "Cannot Reproduce", 
+            "https://bugs.mojang.com/rest/api/2/resolution/1": "Fixed",
+            "https://bugs.mojang.com/rest/api/2/resolution/2": "Won't Fix",
+            "https://bugs.mojang.com/rest/api/2/resolution/3": "Duplicate",
+            "https://bugs.mojang.com/rest/api/2/resolution/4": "Incomplete",
+            "https://bugs.mojang.com/rest/api/2/resolution/5": "Cannot Reproduce",
             "https://bugs.mojang.com/rest/api/2/resolution/6": "Works as Intended",
             "https://bugs.mojang.com/rest/api/2/resolution/7": "Invalid",
             "https://bugs.mojang.com/rest/api/2/resolution/10001": "Awaiting Response",
-            "https://bugs.mojang.com/rest/api/2/resolution/10003": "Done"
+            "https://bugs.mojang.com/rest/api/2/resolution/10003": "Done",
         }
 
         self.status_table = {
@@ -27,33 +27,39 @@ class JiraCog(Cog):
             "https://bugs.mojang.com/rest/api/2/status/4": "Reopened",
             "https://bugs.mojang.com/rest/api/2/status/5": "Resolved",
             "https://bugs.mojang.com/rest/api/2/status/6": "Closed",
-            "https://bugs.mojang.com/rest/api/2/status/10200": "Postponed"
+            "https://bugs.mojang.com/rest/api/2/status/10200": "Postponed",
         }
 
     # TODO remove hardcoded status and resolve values and add config for JIRA URL and bug ID format
-    @command(aliases=["jira", "bug"])
+    @command(name="jira", aliases=["bug"])
     async def cmd_jira(self, ctx: discord.Message, bug_id: str):
         # Assume the parameter is a URL, so get the ID from it
         if "/" in bug_id:
             bug_id = bug_id.split("/")[-1]
 
         try:
-            report_data = requests.get(f"https://bugs.mojang.com/rest/api/latest/issue/{bug_id}").json()["fields"]
+            report_data = requests.get(
+                f"https://bugs.mojang.com/rest/api/latest/issue/{bug_id}"
+            ).json()["fields"]
         # Bug report doesn't exist
         except KeyError:
-            await ctx.send(f"**{bug_id.upper()}** is not accessible. This may be due to it being private or it may not exist.")
+            await ctx.send(
+                f"**{bug_id.upper()}** is not accessible. This may be due to it being private or it may not exist."
+            )
             return
 
         title = f"[{bug_id.upper()}] {report_data['summary']}"
-        if report_data["assignee"] == None: 
+        if report_data["assignee"] == None:
             assignee = "Unassigned"
-        else: 
+        else:
             assignee = report_data["assignee"]["name"]
         reporter = report_data["reporter"]["displayName"]
         creation_date = report_data["created"][:10]
         since_version = report_data["versions"][0]["name"]
 
-        jira_embed = discord.Embed(title=title, url=f"https://bugs.mojang.com/browse/{bug_id}", color=0x00aced)
+        jira_embed = discord.Embed(
+            title=title, url=f"https://bugs.mojang.com/browse/{bug_id}", color=0x00ACED
+        )
         jira_embed.add_field(name="Reporter", value=reporter, inline=True)
         jira_embed.add_field(name="Assignee", value=assignee, inline=True)
         jira_embed.add_field(name="Created On", value=creation_date, inline=True)
@@ -72,20 +78,22 @@ class JiraCog(Cog):
 
             jira_embed.add_field(name="Status", value=status, inline=True)
             jira_embed.add_field(name="Votes", value=votes, inline=True)
-            #jira_embed.add_field(name="Confirmation", value=confirmation, inline=True)
+            # jira_embed.add_field(name="Confirmation", value=confirmation, inline=True)
 
         # The bug report is closed
         else:
             resolution_status = self.resolution_table[report_data["resolution"]["self"]]
             resolve_date = report_data["resolutiondate"][:10]
-            if not report_data["fixVersions"]: 
+            if not report_data["fixVersions"]:
                 fix_version = "None"
-            else: 
+            else:
                 fix_version = report_data["fixVersions"][0]["name"]
 
             jira_embed.add_field(name="Resolution", value=resolution_status, inline=True)
             jira_embed.add_field(name="Resolved On", value=resolve_date, inline=True)
             jira_embed.add_field(name="Fix Version", value=fix_version, inline=True)
 
-        jira_embed.set_footer(text=str(ctx.message.author), icon_url=str(ctx.message.author.avatar_url))
+        jira_embed.set_footer(
+            text=str(ctx.message.author), icon_url=str(ctx.message.author.avatar_url)
+        )
         await ctx.send(embed=jira_embed)
