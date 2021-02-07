45152aebbc2b95519f1be14a034fdfc1adf183f5
DoubleF3lix <48461905+Double-Felix@users.noreply.github.com>
2021-02-07 14:07:42 -0600

    Modified according to review

--- commanderbot_ext/ping/ping_cog.py
+++ commanderbot_ext/ping/ping_cog.py
@@ -1,13 +1,10 @@
-import discord
-from commanderbot_lib.logging import Logger, get_clogger
 from discord.ext.commands import Bot, Cog, Context, command
 
 
 class PingCog(Cog):
     def __init__(self, bot: Bot):
         self.bot: Bot = bot
-        self._log: Logger = get_clogger(self)
 
     @command(name="ping")
-    async def cmd_ping(self, ctx: discord.Message):
-        await ctx.send(f"Pong! Latency: {round(self.bot.latency, 2)}ms")
+    async def cmd_ping(self, ctx: Context):
+        await ctx.send(f"Pong! Latency: {round(self.bot.latency, 2)}s")
