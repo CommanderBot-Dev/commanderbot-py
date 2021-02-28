c99a988462d51f35023bd13b9a4963647d49ac20
Valentin Berlier <berlier.v@gmail.com>
2021-02-28 09:20:02 +0100

    Add error handler

--- commanderbot_ext/pack/pack_cog.py
+++ commanderbot_ext/pack/pack_cog.py
@@ -1,6 +1,10 @@
 import asyncio
 import io
+from contextlib import asynccontextmanager
+from typing import AsyncIterator
 
+from beet import FormattedPipelineException
+from beet.toolchain.utils import format_exc
 from commanderbot_lib.logging import Logger, get_clogger
 from discord import File, Message
 from discord.ext.commands import Bot, Cog, Context, command
@@ -25,17 +29,37 @@ class PackCog(Cog):
         self._log.info("%s: Running build for %s.", message.id, author)
 
         loop = asyncio.get_running_loop()
-        attachments = await loop.run_in_executor(
-            None, generate_packs, author, message.content
-        )
 
-        if attachments:
-            files = [
-                File(io.BytesIO(data), filename=filename)
-                for filename, data in attachments.items()
-            ]
-            await ctx.send(files=files)
-        else:
-            await message.add_reaction("🤔")
+        async with self.error_handler(ctx):
+            attachments = await loop.run_in_executor(
+                None, generate_packs, author, message.content
+            )
+
+            if attachments:
+                files = [
+                    File(io.BytesIO(data), filename=filename)
+                    for filename, data in attachments.items()
+                ]
+                await ctx.send(files=files)
+            else:
+                await message.add_reaction("🤔")
 
         self._log.info("%s: Done.", message.id)
+
+    @asynccontextmanager
+    async def error_handler(self, ctx: Context) -> AsyncIterator[None]:
+        try:
+            yield
+        except FormattedPipelineException as exc:
+            message = exc.message
+            exception = exc.__cause__ if exc.format_cause else None
+        except Exception as exc:
+            message = "An unhandled exception occurred. This could be a bug."
+            exception = exc
+        else:
+            return
+
+        if exception:
+            message += f"\n```{format_exc(exception)}```"
+
+        await ctx.send(message)
