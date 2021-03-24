3e260f5d7dae79f4f460850368d689f047ef1de2
Arcensoth <arcensoth@gmail.com>
2021-03-24 11:16:03 -0400

    Explain CogGuildStateFactory

--- commanderbot_ext/_lib/cog_guild_state_factory.py
+++ commanderbot_ext/_lib/cog_guild_state_factory.py
@@ -9,6 +9,14 @@ GuildStateType = TypeVar("GuildStateType", bound=CogGuildState)
 
 
 class CogGuildStateFactory(Protocol[GuildStateType]):
+    """
+    An arbitrary callable object that creates new guild states.
+
+    This is an implicit callable interface, and there are two ways to use it:
+    1. pass a function with the same signature as `__call__`; or
+    2. pass an instance of a class that implements `__call__` with the same signature.
+    """
+
     @abstractmethod
     def __call__(self, guild: Guild) -> GuildStateType:
         ...
