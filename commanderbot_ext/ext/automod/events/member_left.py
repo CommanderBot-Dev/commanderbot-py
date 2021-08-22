de96316325df5894308600b7845da92ea199a5fc
Arcensoth <arcensoth@gmail.com>
2021-08-22 18:16:34 -0400

    Add extra fields to member join/leave events

--- commanderbot_ext/ext/automod/events/member_left.py
+++ commanderbot_ext/ext/automod/events/member_left.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass
+from typing import Any, Iterable, Tuple
 
 from discord import Member
 
 from commanderbot_ext.ext.automod.automod_event import AutomodEventBase
+from commanderbot_ext.lib.utils import yield_member_date_fields
 
 __all__ = ("MemberLeft",)
 
@@ -14,3 +16,6 @@ class MemberLeft(AutomodEventBase):
     @property
     def member(self) -> Member:
         return self._member
+
+    def _yield_extra_fields(self) -> Iterable[Tuple[str, Any]]:
+        yield from yield_member_date_fields(self.member)
