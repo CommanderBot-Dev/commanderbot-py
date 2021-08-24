4db4ddecec42e3c3f885f3b01318124133b2ce7d
Arcensoth <arcensoth@gmail.com>
2021-08-24 00:35:04 -0400

    Disable pings by default in ResponsiveException

--- commanderbot_ext/lib/responsive_exception.py
+++ commanderbot_ext/lib/responsive_exception.py
@@ -1,8 +1,40 @@
+from typing import Optional
+
+from discord import AllowedMentions
 from discord.ext.commands import Context
 
 __all__ = ("ResponsiveException",)
 
 
 class ResponsiveException(Exception):
-    async def respond(self, ctx: Context):
-        await ctx.reply(str(self))
+    def __init__(
+        self,
+        *args,
+        allowed_mentions: Optional[AllowedMentions] = None,
+    ):
+        self.allowed_mentions: Optional[AllowedMentions] = allowed_mentions
+        super().__init__(*args)
+
+    @classmethod
+    def allowed_mentions_default_factory(cls) -> AllowedMentions:
+        return AllowedMentions(
+            everyone=False,
+            users=False,
+            roles=False,
+            replied_user=True,
+        )
+
+    async def respond(
+        self,
+        ctx: Context,
+        allowed_mentions: Optional[AllowedMentions] = None,
+    ):
+        allowed_mentions = (
+            allowed_mentions
+            or self.allowed_mentions
+            or self.allowed_mentions_default_factory()
+        )
+        await ctx.reply(
+            str(self),
+            allowed_mentions=allowed_mentions,
+        )
