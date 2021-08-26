ce869045335c6537bbf69998e139dd6435ebfdec
Ersatz <cm12mc@gmail.com>
2021-08-25 19:44:16 -0700

    Modified vote command so it reacts with emojis in the order they appeared

--- commanderbot_ext/ext/vote/vote_cog.py
+++ commanderbot_ext/ext/vote/vote_cog.py
@@ -1,10 +1,12 @@
+import re
 from typing import Iterable, cast
 
+import emoji
 from discord import Message
 from discord.ext.commands import Bot, Cog, Context, command
-from emoji.core import distinct_emoji_lis
 
 DEFAULT_VOTE_EMOJIS = ("👍", "👎")
+CUSTOM_EMOJI_PATTERN = re.compile(r"\<\:\w+\:\d+\>")
 
 
 class VoteCog(Cog, name="commanderbot_ext.ext.vote"):
@@ -14,14 +16,27 @@ class VoteCog(Cog, name="commanderbot_ext.ext.vote"):
     @staticmethod
     def get_emojis(message: Message) -> Iterable[str]:
         # Get message content and cast it to a string
-        message_content = str(message.clean_content)
+        message_content: str = str(message.clean_content)
 
-        # If message contents had any emojis, return them
-        if emojis := distinct_emoji_lis(message_content):
-            return emojis
+        # Find unicode and custom emojis in the message
+        found_emojis: list[dict] = emoji.emoji_lis(message_content)
+        for custom_emoji in CUSTOM_EMOJI_PATTERN.finditer(message_content):
+            found_emojis.append(
+                {"location": custom_emoji.start(), "emoji": custom_emoji.group()}
+            )
+        
+        # Return early with the default emojis if no emojis were found
+        if not found_emojis:
+            return DEFAULT_VOTE_EMOJIS
 
-        # Return default emojis if none were found
-        return DEFAULT_VOTE_EMOJIS
+        # Create a list of unique emojis that are sorted in the order they appeared
+        emojis: list[str] = []
+        for e in sorted(found_emojis, key=lambda i: i["location"]):
+            emoji_char: str = str(e["emoji"])
+            if emoji_char not in emojis:
+                emojis.append(emoji_char)
+
+        return emojis
 
     @command(name="vote")
     async def cmd_vote(self, ctx: Context):
