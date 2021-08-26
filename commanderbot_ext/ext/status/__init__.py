7bff0eb6b15d2969ea02dfac124b4d7731f79442
Arcensoth <arcensoth@gmail.com>
2021-08-26 13:25:00 -0400

    Update status

--- commanderbot_ext/ext/status/__init__.py
+++ commanderbot_ext/ext/status/__init__.py
@@ -1,7 +1,7 @@
-from discord.ext import commands
+from discord.ext.commands import Bot
 
 from commanderbot_ext.ext.status.status_cog import StatusCog
 
 
-def setup(bot: commands.Bot):
+def setup(bot: Bot):
     bot.add_cog(StatusCog(bot))
