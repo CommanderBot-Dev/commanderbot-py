23a08a1c0daca97701216cc9562da51c970f11b5
Arcensoth <arcensoth@gmail.com>
2021-08-26 15:52:44 -0400

    Update kick

--- commanderbot_ext/ext/kick/__init__.py
+++ commanderbot_ext/ext/kick/__init__.py
@@ -1,7 +1,7 @@
-from discord.ext import commands
+from discord.ext.commands import Bot
 
 from commanderbot_ext.ext.kick.kick_cog import KickCog
 
 
-def setup(bot: commands.Bot):
+def setup(bot: Bot):
     bot.add_cog(KickCog(bot))
--- commanderbot_ext/ext/kick/kick_cog.py
+++ commanderbot_ext/ext/kick/kick_cog.py
@@ -1,12 +1,9 @@
-import logging
 from typing import Optional
 
 from discord import Member
 from discord.ext.commands import Bot, Cog, Context, command, has_permissions
 
 from commanderbot_ext.lib import checks
-
-LOG = logging.getLogger(__name__)
 
 
 class KickCog(Cog, name="commanderbot_ext.ext.kick"):
@@ -25,8 +22,7 @@ class KickCog(Cog, name="commanderbot_ext.ext.kick"):
     ):
         # make sure we aren't trying to kick the bot itself
         if user == self.bot.user:
-            await ctx.reply("I don't think you want to do that...")
-            LOG.warning("Tried to kick the bot itself")
+            await ctx.message.reply("I don't think you want to do that...")
             return
 
         # attempt to DM if a reason was included
@@ -38,15 +34,11 @@ class KickCog(Cog, name="commanderbot_ext.ext.kick"):
                 )
                 await ctx.message.add_reaction("✉️")
             except:
-                LOG.exception(f"Failed to DM {user} about being kicked")
-            else:
-                LOG.info(f"Successfully DM'd {user} about being kicked")
+                pass
 
         # actually kick the user
         try:
             await user.kick(reason=reason)
             await ctx.message.add_reaction("👢")
         except:
-            LOG.exception(f"Failed to kick {user}")
-        else:
-            LOG.info(f"Successfully kicked {user}")
+            await ctx.message.add_reaction("🔥")
