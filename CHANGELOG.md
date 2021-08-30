356053ccc9a7d6c675cb37d0c435bf1add58fa86
Arcensoth <arcensoth@gmail.com>
2021-08-30 11:58:05 -0400

    Add `allowed_mentions` to `reply_to_message` action

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased]
+
+### Added
+
+- Added `allowed_mentions` to `reply_to_message` action
+
 ## [0.16.0] - 2021-08-30
 
 ### Changed
--- commanderbot/ext/automod/actions/reply_to_message.py
+++ commanderbot/ext/automod/actions/reply_to_message.py
@@ -1,8 +1,11 @@
 from dataclasses import dataclass
+from typing import Optional, Type, TypeVar
 
 from commanderbot.ext.automod.automod_action import AutomodAction, AutomodActionBase
 from commanderbot.ext.automod.automod_event import AutomodEvent
-from commanderbot.lib import JsonObject
+from commanderbot.lib import AllowedMentions, JsonObject
+
+ST = TypeVar("ST")
 
 
 @dataclass
@@ -14,14 +17,31 @@ class ReplyToMessage(AutomodActionBase):
     ----------
     content
         The content of the message to send.
+    allowed_mentions
+        The types of mentions allowed in the message. Unless otherwise specified, only
+        "everyone" mentions will be suppressed.
     """
 
     content: str
+    allowed_mentions: Optional[AllowedMentions] = None
+
+    @classmethod
+    def from_data(cls: Type[ST], data: JsonObject) -> ST:
+        allowed_mentions = AllowedMentions.from_field_optional(data, "allowed_mentions")
+        return cls(
+            description=data.get("description"),
+            content=data.get("content"),
+            allowed_mentions=allowed_mentions,
+        )
 
     async def apply(self, event: AutomodEvent):
         if message := event.message:
             content = event.format_content(self.content)
-            await message.reply(content)
+            allowed_mentions = self.allowed_mentions or AllowedMentions.not_everyone()
+            await message.reply(
+                content,
+                allowed_mentions=allowed_mentions,
+            )
 
 
 def create_action(data: JsonObject) -> AutomodAction:
