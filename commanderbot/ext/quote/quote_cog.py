89085ddf92f788ca8f46fbc01e3f83b0d250a096
Arcensoth <arcensoth@gmail.com>
2021-08-30 15:21:22 -0400

    Overhaul `quote` format, and account for attachments

--- commanderbot/ext/quote/quote_cog.py
+++ commanderbot/ext/quote/quote_cog.py
@@ -1,4 +1,14 @@
-from discord import AllowedMentions, Embed, Message
+from typing import List, Optional
+
+from discord import (
+    AllowedMentions,
+    Attachment,
+    Embed,
+    File,
+    Message,
+    TextChannel,
+    Thread,
+)
 from discord.ext.commands import Bot, Cog, Context, command
 
 
@@ -6,53 +16,93 @@ class QuoteCog(Cog, name="commanderbot.ext.quote"):
     def __init__(self, bot: Bot):
         self.bot: Bot = bot
 
-    async def construct_embed(self, ctx: Context, message: Message) -> Embed:
-        quote_embed = Embed(
-            description=message.clean_content,
-            timestamp=message.created_at,
-        )
-        quote_embed.set_footer(
-            text=f"{message.author} in #{message.channel}",
-            icon_url=message.author.display_avatar.url,
-        )
-        return quote_embed
-
     async def do_quote(
         self,
         ctx: Context,
         message: Message,
-        content: str,
+        phrasing: str,
         allowed_mentions: AllowedMentions,
     ):
-        quote_embed = await self.construct_embed(ctx, message)
-        await ctx.send(
-            content,
-            embed=quote_embed,
-            allowed_mentions=allowed_mentions,
+        if not isinstance(message.channel, TextChannel | Thread):
+            await ctx.message.reply("😔 I can't quote that.")
+            return
+
+        # Build the message content containing the quote metadata.
+        created_at_ts = int(message.created_at.timestamp())
+        quote_ts = f"<t:{created_at_ts}:R>"
+        if message.edited_at is not None:
+            edited_at_ts = int(message.edited_at.timestamp())
+            quote_ts += f" (edited <ts:{edited_at_ts}:R>)"
+        content = (
+            f"{ctx.author.mention} {phrasing} {message.author.mention}"
+            + f" in {message.channel.mention} from {quote_ts}:"
+            + f"\n{message.jump_url}"
         )
+
+        # Build the embed containing the actual quote content, if any.
+        quote_embed: Optional[Embed] = None
+        original_content = message.content
+        if original_content:
+            quote_embed = Embed(
+                description=original_content,
+            )
+            quote_embed.set_author(
+                name=str(message.author),
+                icon_url=message.author.display_avatar.url,
+            )
+
+        # Account for any attachments to the original message.
+        quote_files: List[File] = []
+        failed_attachments: List[Attachment] = []
+        for att in message.attachments:
+            try:
+                file = await att.to_file()
+            except:
+                failed_attachments.append(att)
+            else:
+                quote_files.append(file)
+
+        # Send the message with the embed attached, if any.
+        if quote_embed:
+            await ctx.send(
+                content,
+                embed=quote_embed,
+                files=quote_files,
+                allowed_mentions=allowed_mentions,
+            )
+        elif quote_files:
+            await ctx.send(
+                content,
+                files=quote_files,
+                allowed_mentions=allowed_mentions,
+            )
+        else:
+            await ctx.message.reply("🤔 There's nothing to quote.")
+            return
+
+        # Let the user know if any attachments failed.
+        if failed_attachments:
+            await ctx.message.add_reaction("⚠️")
 
     @command(name="quote")
     async def cmd_quote(self, ctx: Context, message: Message):
-        content = (
-            f"{ctx.author.mention} quoted {message.author.mention} {message.jump_url}"
-        )
-        allowed_mentions = AllowedMentions.none()
         await self.do_quote(
-            ctx, message, content=content, allowed_mentions=allowed_mentions
+            ctx,
+            message,
+            phrasing="quoted",
+            allowed_mentions=AllowedMentions.none(),
         )
 
     @command(name="quotem")
     async def cmd_quotem(self, ctx: Context, message: Message):
-        content = (
-            f"{ctx.author.mention} quote-mentioned {message.author.mention}"
-            + f" {message.jump_url}"
-        )
-        allowed_mentions = AllowedMentions(
-            everyone=False,
-            users=True,
-            roles=False,
-            replied_user=False,
-        )
         await self.do_quote(
-            ctx, message, content=content, allowed_mentions=allowed_mentions
+            ctx,
+            message,
+            phrasing="quote-mentioned",
+            allowed_mentions=AllowedMentions(
+                everyone=False,
+                users=True,
+                roles=False,
+                replied_user=False,
+            ),
         )
