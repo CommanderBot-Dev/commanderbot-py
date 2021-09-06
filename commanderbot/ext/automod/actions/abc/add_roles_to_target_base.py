fb357243b47a48b643e4dc3db733fdb5c2b26fb7
Arcensoth <arcensoth@gmail.com>
2021-09-06 13:17:39 -0400

    `automod`: Add optional reason to roles assignment actions

--- commanderbot/ext/automod/actions/abc/add_roles_to_target_base.py
+++ commanderbot/ext/automod/actions/abc/add_roles_to_target_base.py
@@ -11,6 +11,7 @@ from commanderbot.lib import RoleID
 @dataclass
 class AddRolesToTargetBase(AutomodActionBase):
     roles: Tuple[RoleID]
+    reason: Optional[str] = None
 
     def get_target(self, event: AutomodEvent) -> Optional[Member]:
         raise NotImplementedError()
@@ -21,4 +22,4 @@ class AddRolesToTargetBase(AutomodActionBase):
             # TODO Warn about unresolved roles. #logging
             roles = [guild.get_role(role_id) for role_id in self.roles]
             roles = [role for role in roles if role]
-            await member.add_roles(*roles)
+            await member.add_roles(*roles, reason=self.reason)
