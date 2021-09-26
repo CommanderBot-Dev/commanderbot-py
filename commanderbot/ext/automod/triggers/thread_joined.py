9c1119e0a1a5261f65d54aeb4649793d9e73e34f
Arcensoth <arcensoth@gmail.com>
2021-09-26 17:51:55 -0400

    `automod`: Add `thread_joined` trigger

---
+++ commanderbot/ext/automod/triggers/thread_joined.py
@@ -0,0 +1,59 @@
+from dataclasses import dataclass
+from typing import Optional, Type, TypeVar
+
+from discord import Thread
+
+from commanderbot.ext.automod import events
+from commanderbot.ext.automod.automod_event import AutomodEvent
+from commanderbot.ext.automod.automod_trigger import AutomodTrigger, AutomodTriggerBase
+from commanderbot.lib import ChannelsGuard, JsonObject
+
+ST = TypeVar("ST")
+
+
+@dataclass
+class ThreadJoined(AutomodTriggerBase):
+    """
+    Fires when an `on_thread_join` event is received.
+
+    See: https://discordpy.readthedocs.io/en/master/api.html#discord.on_thread_join
+
+    Attributes
+    ----------
+    parent_channels
+        The parent channels to match against. If empty, all channels will match.
+    """
+
+    event_types = (events.ThreadJoined,)
+
+    parent_channels: Optional[ChannelsGuard] = None
+
+    @classmethod
+    def from_data(cls: Type[ST], data: JsonObject) -> ST:
+        parent_channels = ChannelsGuard.from_field_optional(data, "parent_channels")
+        return cls(
+            description=data.get("description"),
+            parent_channels=parent_channels,
+        )
+
+    def ignore(self, event: AutomodEvent) -> bool:
+        thread = event.channel
+
+        # If the channel is not a thread, ignore it.
+        if not isinstance(thread, Thread):
+            return True
+
+        # If no parent channels are defined, don't ignore the thread.
+        if self.parent_channels is None:
+            return False
+
+        # If the thread has no parent, ignore it.
+        if not thread.parent:
+            return True
+
+        # Otherwise, ignore the thread according to its parent.
+        return self.parent_channels.ignore(thread.parent)
+
+
+def create_trigger(data: JsonObject) -> AutomodTrigger:
+    return ThreadJoined.from_data(data)
