00990dea7180276a0145309a609e1a53927c4df0
DoubleF3lix <48461905+Double-Felix@users.noreply.github.com>
2021-11-08 11:24:12 -0600

    Updated allay version, added better error messages, error is now printed to the user

--- commanderbot/ext/allay/allay_cog.py
+++ commanderbot/ext/allay/allay_cog.py
@@ -2,10 +2,13 @@ from textwrap import dedent
 
 import allay
 from discord.ext.commands import Bot, Cog, Context, command
+from tokenstream.error import InvalidSyntax
+
+from commanderbot.lib.responsive_exception import ResponsiveException
 
 CMD_HELP = """
     Convert plaintext into a text-component via Allay
-    Usage: https://github.com/DoubleF3lix/Allay
+    Usage: https://github.com/DoubleF3lix/Allay#format
 
     Example usage:
 
@@ -86,7 +89,12 @@ class AllayCog(Cog, name="commanderbot.ext.allay"):
 
         if code_block:
             # Parse the output and print it in a code block (None is fine to pass)
-            parsed_contents = self.parser.parse(code_block, indent)
+            try:
+                parsed_contents = self.parser.parse(code_block, indent)
+            except Exception as error:
+                raise ResponsiveException(
+                    f"```\n{error}\n```\nUsage: <https://github.com/DoubleF3lix/Allay#format>"
+                ) from error
             await ctx.send("```json\n" + parsed_contents + "\n```")
         else:
             await ctx.reply(
