a17c5b93434a36bbd9c13683eaf0d25494227801
Ersatz <cm12mc@gmail.com>
2023-01-10 14:30:04 -0800

    Fixed an issue where syncing guild commands in a DM would throw an exception

--- commanderbot/ext/sudo/sudo_exceptions.py
+++ commanderbot/ext/sudo/sudo_exceptions.py
@@ -21,7 +21,7 @@ class SyncError(SudoException):
 
 class SyncUnknownGuild(SudoException):
     def __init__(self, guild: Optional[Object]):
-        super().__init__(f"**Unknown guild:**\nGuild ID: `{guild.id}`" if guild else "")
+        super().__init__(f"**Unknown guild:**\nGuild ID: `{guild.id if guild else '❓'}`")
 
 
 class UnknownCog(SudoException):
